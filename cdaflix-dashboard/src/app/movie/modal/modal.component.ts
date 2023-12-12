05e6c83f951bcf94fb63775f743887c3022434b4
Hamidou.Sow <hamidou.sow@hotmail.fr>
2023-12-12 15:07:30 +0100

    add angular bootstrap

---
+++ cdaflix-dashboard/src/app/movie/modal/modal.component.ts
@@ -0,0 +1,50 @@
+import { Component, inject, TemplateRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss'
+})
+export class ModalComponent {
+
+    private modalService = inject(NgbModal);
+  
+    openBackDropCustomClass(content: TemplateRef<any>) {
+      this.modalService.open(content, { backdropClass: 'light-blue-backdrop' });
+    }
+  
+    openWindowCustomClass(content: TemplateRef<any>) {
+      this.modalService.open(content, { windowClass: 'dark-modal' });
+    }
+  
+    openSm(content: TemplateRef<any>) {
+      this.modalService.open(content, { size: 'sm' });
+    }
+  
+    openLg(content: TemplateRef<any>) {
+      this.modalService.open(content, { size: 'lg' });
+    }
+  
+    openXl(content: TemplateRef<any>) {
+      this.modalService.open(content, { size: 'xl' });
+    }
+  
+    openVerticallyCentered(content: TemplateRef<any>) {
+      this.modalService.open(content, { centered: true });
+    }
+  
+    openScrollableContent(longContent: any) {
+      this.modalService.open(longContent, { scrollable: true });
+    }
+  
+    openModalDialogCustomClass(content: TemplateRef<any>) {
+      this.modalService.open(content, { modalDialogClass: 'dark-modal' });
+    }
+  
+}
+
+
