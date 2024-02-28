14923262ab4e9b08c8fdeebdf342ae66aafdd6ff
Hamidou.Sow <hamidou.sow@hotmail.fr>
2024-02-28 17:30:23 +0100

    add movie suggestions for each movie

--- cdaflix-front/src/app/public/shared-public/components/movie-modal/movie-modal.component.ts
+++ cdaflix-front/src/app/public/shared-public/components/movie-modal/movie-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Signal, TemplateRef, WritableSignal, inject } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Signal, TemplateRef, WritableSignal, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { TmdbService } from '../../../../services/tmdb-service/tmdb.service';
@@ -6,12 +6,16 @@ import { tmdbUtil } from '../../../../utils/tmdb-util';
 import { TmdbMovieDetails } from '../../../../models/TmdbMovieDetails';
 import { MovieProvider } from '../../../../models/MovieProvider';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CardComponent } from '../card/card.component';
 
 
 @Component({
   selector: 'app-movie-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule, 
+    CardComponent,
+  ],
   templateUrl: './movie-modal.component.html',
   styleUrl: './movie-modal.component.scss'
 })
@@ -22,23 +26,33 @@ export class MovieModalComponent implements OnInit {
   private modalService = inject(NgbModal)
 
   @Input()
-  movie: TmdbMovieDetails | null = this.movieService.movieDetails$();
+  movie: TmdbMovieDetails = this.movieService.movieDetails$();
   
   imageBaseurl = tmdbUtil.imageBaseUrl;
   
   movieProviders = this.movieService.movieProviders$;
+
+  moviesSuggestions = this.movieService.moviesSuggestions$;
 
   languageSelected = 'CA'
 
   closeModal() {
     this.modalService.dismissAll()
   } 
+
+  // switchModal() {
+  //   if(this.modalService.hasOpenModals()) {
+  //     this.modalService.dismissAll()
+  //     this.modalService.open()
+  //   }
+  // }
   
 
   ngOnInit() {
     const id = this.movie?.id
     if(id != null) {
       this.movieService.getMovieProviders(id);
+      this.movieService.getMovieSuggestions(id);
     } 
   }
 
