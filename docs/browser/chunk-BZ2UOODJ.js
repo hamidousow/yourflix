95d156c6794cbdfcf60241ca27bd8662954ae96e
Hamidou.Sow <hamidou.sow@hotmail.fr>
2024-03-01 01:51:30 +0100

    build local

---
+++ docs/browser/chunk-BZ2UOODJ.js
@@ -0,0 +1,1 @@
+import{ac as m,cb as i,da as n,db as a,qb as r,wb as s}from"./chunk-X2FQETT2.js";var l=(()=>{let e=class e{};e.\u0275fac=function(o){return new(o||e)},e.\u0275cmp=n({type:e,selectors:[["app-movies-view"]],standalone:!0,features:[s],decls:2,vars:0,template:function(o,d){o&1&&(i(0,"p"),r(1,"movies-view works!"),a())},dependencies:[m]});let t=e;return t})();export{l as default};
