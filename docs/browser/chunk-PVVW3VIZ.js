afdc873225a256179a22bde6071be36049e70e19
Hamidou.Sow <hamidou.sow@hotmail.fr>
2024-03-01 16:56:13 +0100

    remove docs

--- docs/browser/chunk-PVVW3VIZ.js
+++
@@ -1,1 +0,0 @@
-import{f as a}from"./chunk-XKGJZN4A.js";import"./chunk-4TINCANC.js";import"./chunk-DRVLGHJP.js";import"./chunk-YEQ72UKM.js";export{a as default};
--- docs/browser/chunk-R7JHJXZD.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-7HZ7RF36.js";import"./chunk-XKGJZN4A.js";import"./chunk-N2USPGYN.js";import"./chunk-4TINCANC.js";import"./chunk-DRVLGHJP.js";import"./chunk-YEQ72UKM.js";export{a as default};
